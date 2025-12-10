62dc7d5da5ebf4c613694b653560551b9cb34446
Renat Gubaev <gubaev.r@n-t.io>
2025-12-10 17:06:16 +0300

    auto commit

--- src/CoinAmount.ts
+++ src/CoinAmount.ts
@@ -1,8 +1,13 @@
-import { IsString, IsNumberString } from 'class-validator';
+import { IsString, IsNumberString, Matches } from 'class-validator';
+import { CoinUtil } from './CoinUtil';
 
 export interface ICoinAmount {
     value: string;
     coinUid: string;
+}
+
+export interface ICoinObjectAmount extends ICoinAmount {
+    objectUid: string;
 }
 
 export class CoinAmount implements ICoinAmount {
@@ -11,4 +16,9 @@ export class CoinAmount implements ICoinAmount {
 
     @IsString()
     public coinUid: string;
+}
+
+export class CoinObjectAmount extends CoinAmount {
+    @Matches(CoinUtil.OBJECT_UID_REG_EXP)
+    public objectUid: string;
 }
