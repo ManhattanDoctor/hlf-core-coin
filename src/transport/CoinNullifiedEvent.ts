f252c224b43872ee3616bcc98b5bb73812e46f05
Renat Gubaev <gubaev.r@n-t.io>
2025-12-10 17:13:06 +0300

    no message

---
+++ src/transport/CoinNullifiedEvent.ts
@@ -0,0 +1,37 @@
+import { TransformUtil } from "@ts-core/common";
+import { Matches } from 'class-validator';
+import { CoinEvent, ICoinEventDto } from "./CoinEvent";
+import { CoinUtil } from "../CoinUtil";
+import { Event } from './Event';
+
+export class CoinNullifiedEvent extends CoinEvent<ICoinNullifiedEventDto> {
+    // --------------------------------------------------------------------------
+    //
+    //  Public Static Properties
+    //
+    // --------------------------------------------------------------------------
+
+    public static readonly NAME = Event.COIN_NULLIFIED;
+
+    // --------------------------------------------------------------------------
+    //
+    //  Constructor
+    //
+    // --------------------------------------------------------------------------
+
+    constructor(data: ICoinNullifiedEventDto) {
+        super(CoinNullifiedEvent.NAME, TransformUtil.toClass(CoinNullifiedEventDto, data));
+    }
+}
+export interface ICoinNullifiedEventDto extends ICoinEventDto {
+    objectUid: string;
+}
+export class CoinNullifiedEventDto implements ICoinNullifiedEventDto {
+    @Matches(CoinUtil.UID_REG_EXP)
+    public coinUid: string;
+
+    @Matches(CoinUtil.OBJECT_UID_REG_EXP)
+    public objectUid: string;
+}
+
+
---
+++ src/transport/CoinNullifyCommand.ts
@@ -0,0 +1,44 @@
+
+import { TransformUtil } from '@ts-core/common';
+import { Matches, IsString, IsOptional } from 'class-validator';
+import { HlfTransportCommandAsync, InitiatedDto } from '@hlf-core/common';
+import { CommandName } from './Command';
+import { CoinUtil } from '../CoinUtil';
+
+export class CoinNullifyCommand extends HlfTransportCommandAsync<ICoinNullifyDto, void> {
+    // --------------------------------------------------------------------------
+    //
+    //  Public Static Properties
+    //
+    // --------------------------------------------------------------------------
+
+    public static readonly NAME = CommandName.COIN_NULLIFY;
+
+    // --------------------------------------------------------------------------
+    //
+    //  Constructor
+    //
+    // --------------------------------------------------------------------------
+
+    constructor(request: ICoinNullifyDto) {
+        super(CoinNullifyCommand.NAME, TransformUtil.toClass(CoinNullifyDto, request));
+    }
+}
+
+export interface ICoinNullifyDto extends InitiatedDto {
+    coinUid: string;
+    objectUid: string;
+    transactionHash?: string;
+}
+
+export class CoinNullifyDto extends InitiatedDto {
+    @Matches(CoinUtil.UID_REG_EXP)
+    public coinUid: string;
+
+    @Matches(CoinUtil.OBJECT_UID_REG_EXP)
+    public objectUid: string;
+
+    @IsString()
+    @IsOptional()
+    public transactionHash?: string;
+}
